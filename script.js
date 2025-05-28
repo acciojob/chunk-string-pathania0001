a0f743ee7c103720bfb0fe0d8aa13a8e86ff42f6
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-05-28 18:14:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // script.js
 function stringChop(str, size) {
-  let res = [];
+    if (str == null || size == null) return [];
 
   str = str.toString();
   size = parseInt(size);
-  if(str =="")
-	  return [];
+  
+  if (isNaN(size) || size <= 0) return [];
+
+  let res = [];
   for (let i = 0; i < str.length; i += size) {
     res.push(str.slice(i, i + size));
   }
